5c75223b50e4fc660b5c3d98cdd40e620c6c97e6
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-09-26 16:34:28 -0400

    Added bin detail component

--- client/components/bins/BinsMain.jsx
+++ client/components/bins/BinsMain.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../imports/collections/bins';
 class BinsMain extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    console.log(this.props.bin)
     return (
       <div>Bins main</div>
     )
   }
 }
 
-export default BinsMain;
+export default createContainer((props) => {
+ const { binId } = props.params;
+ Meteor.subscribe('bins');
+ return {
+   bin: Bins.findOne(binId)
+ }
+}, BinsMain) ;
